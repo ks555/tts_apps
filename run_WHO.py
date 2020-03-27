f657b5d1a696034e3a127e6ef642dc51f03506f0
Kristen Scott <kristenmscott@hotmail.com>
2020-03-27 17:27:32 +0000

    run_who.py can now parse the blog posts into seperate useable texts

---
+++ run_WHO.py
@@ -0,0 +1,94 @@
+# -*- coding: UTF-8 -*
+import time
+import datetime
+import argparse
+from weather.yr_forecast import YrForecast
+import utils.utils as utils
+import requests
+from bs4 import BeautifulSoup
+from readability import Document
+from random import randrange
+from lxml import etree
+
+
+def get_content(url):
+    response = requests.get(url)
+    doc = Document(response.text)
+    #print(doc.title())
+    summary = doc.summary(html_partial=True)
+    summary = summary.encode('UTF-8')
+    entry_list = summary.split('<div class="sf-content-block content-block">')
+    root = etree.fromstring(summary)
+    #print(etree.tostring(root))
+
+        #print(child.tag)
+        
+
+    for child in root: 
+        #find all identified 'posts' (content blocks)   
+        if child.attrib["class"] == "sf-content-block content-block":
+            #go through each element in the content section and add trailing whitespace to ensure proper spacing when combined
+            for element in child.iter():
+                if (element.text): element.text = element.text + " "
+            print(child.xpath("string()").encode('UTF-8'))
+
+    #utils.get_cprc_tts(story, '/audio',  sample_rate=48000, accent='Irish')
+
+
+
+get_content('https://www.who.int/emergencies/diseases/novel-coronavirus-2019/events-as-they-happen')
+#help(Document)
+# # times to run updates based on YR forecast interval times
+# def check_if_update_time(current):
+#     update_times = [[datetime.time(5, 55), datetime.time(5, 58)], [datetime.time(11, 55), datetime.time(11, 58)],\
+#      [datetime.time(17, 55), datetime.time(17, 58)], [datetime.time(23, 55), datetime.time(23, 58)]]
+#     for update_time in update_times:
+#         if current >= update_time[0] and current < update_time[1]:
+#             return True
+#     return False
+
+
+def monitor(forecast):
+    # generate initial forecast audio and text
+    forecast = YrForecast(args.station, args.language, args.gender, args.accent, args.strict_gender, \
+                args.strict_accent, args.sample_rate, args.audio_format, args.metadata, args.time_frame_count)
+    updated = False
+    while True: # loop to maintain script active and monitoring
+        # continously check whether it is update time (four periods daily, based on local time zone)
+        while check_if_update_time(utils.get_local_time(forecast.tz)[0].time()) == False:
+        #while utils.get_local_time(forecast.tz)[0].hour not in [5,11,17,23] and utils.get_local_time(forecast.tz)[0].minute not in range(44, 47):    
+            updated = False
+            time.sleep(60)
+        # If it is updated time, complete forecast update     
+        if updated == False:
+            try:
+                # re-generate audio and text
+                forecast.generate_forecast_string()
+                forecast.generate_forecast_audio()
+                updated = True
+            except:
+            # if update fails, try again
+            # !!Need alert mechanism if no update is made during the time period
+                pass
+        # wait 60 seconds before checking time again
+        time.sleep(60)
+
+
+
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(description='Generates wav file based on current forecast on yr.no')
+#     parser.add_argument('station', type=str, help='station location code (cu, ma)')
+#     parser.add_argument('language', type=str, help='language')
+#     parser.add_argument('-g', '--gender', type=str, default='female', help='Preferred gender of speaker)')
+#     parser.add_argument('-a', '--accent', type=str, default=None, help='Preferred gender of speaker)')
+#     parser.add_argument('-sg', '--strict_gender', default=False, type=str, help='is preferred gender strict?')
+#     parser.add_argument('-sa', '--strict_accent', default=False, type=str, help='is preferred accent strict?')
+#     parser.add_argument('-sr', '--sample_rate', default=8000, type=int, help='sample rate')
+#     parser.add_argument('-af', '--audio_format', default='mp3', type=str, help='audio format')
+#     parser.add_argument('-m', '--metadata', default=True, type=str, help='metadata true or false')
+#     parser.add_argument('-t', '--time_frame_count', default=2, type=int, help='number of time frames to forecast')
+
+
+#     args = parser.parse_args()
+
+#     monitor(args)
